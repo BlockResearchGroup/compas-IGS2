d0d9d11cbae6ff270c45bd035d5eb0940bfd827b
Juney Lee <juney.lee@som.com>
2022-10-11 21:53:15 -0400

    main file structure

---
+++ src/compas_igs2/rhino/objects/forceobject.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import Rhino
+from Rhino.Geometry import Point3d
+
+import compas_rhino
+
+from compas.geometry import scale_vector
+from compas.geometry import add_vectors
+from compas.geometry import subtract_vectors
+from compas.geometry import rotate_points_xy
+
+from compas_ui.rhino.objects import RhinoMeshObject
+
+
+class RhinoDiagramOBject(RhinoMeshOBject):
+    pass
