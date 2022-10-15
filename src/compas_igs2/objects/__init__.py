d56686a07502b9f099ce9e805df9b2ad0efe96a8
Juney Lee <juney.lee@som.com>
2022-10-15 09:27:15 -0400

    import typos

--- src/compas_igs2/objects/__init__.py
+++ src/compas_igs2/objects/__init__.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-from .diagramhobject import DiagramObject
+from .diagramobject import DiagramObject
 from .forceobject import ForceObject
 from .formobject import FormObject
 
