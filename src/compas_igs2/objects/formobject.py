2b5abd549a104d663e0a8d44ace2943d0a10429f
brgcode <tom.v.mele@gmail.com>
2022-11-14 21:09:59 +0100

    settings

--- src/compas_igs2/objects/formobject.py
+++ src/compas_igs2/objects/formobject.py
@@ -3,44 +3,47 @@ from __future__ import absolute_import
 from __future__ import division
 
 from compas.colors import Color
+from compas_ui.values import Settings
+from compas_ui.values import StrValue
+from compas_ui.values import BoolValue
+from compas_ui.values import ColorValue
+from compas_ui.values import FloatValue
 from compas_igs2.objects import DiagramObject
 
 
 class FormObject(DiagramObject):
     """Base object for representing a form diagram in the scene."""
 
-    SETTINGS = {
-        "layer": "formdiagram",
-        "show.vertices": True,
-        "show.edges": True,
-        "show.vertexlabels": False,
-        "show.edgelabels": False,
-        "show.forcecolors": True,
-        "show.forcelabels": True,
-        "show.forcepipes": False,
-        "show.constraints": True,
-        "color.vertices": Color.black(),
-        "color.vertexlabels": Color.white(),
-        "color.vertices:is_fixed": Color.red(),
-        "color.vertices:line_constraint": Color.white(),
-        "color.edges": Color.black(),
-        "color.edges:is_ind": Color.cyan(),
-        "color.edges:is_external": Color.green(),
-        "color.edges:is_reaction": Color.green(),
-        "color.edges:is_load": Color.green(),
-        "color.edges:target_force": Color.white(),
-        "color.edges:target_vector": Color.white(),
-        "color.faces": Color.grey().lightened(50),
-        "color.compression": Color.blue(),
-        "color.tension": Color.red(),
-        "scale.forces": None,
-        "tol.edges": 0.01,
-        "tol.forces": 0.01,
-    }
+    SETTINGS = Settings(
+        {
+            "layer": StrValue("formdiagram"),
+            "show.vertices": BoolValue(True),
+            "show.edges": BoolValue(True),
+            "show.vertexlabels": BoolValue(False),
+            "show.edgelabels": BoolValue(False),
+            "show.forcecolors": BoolValue(True),
+            "show.forcelabels": BoolValue(True),
+            "show.forcepipes": BoolValue(False),
+            "show.constraints": BoolValue(True),
+            "color.vertices": ColorValue(Color.black()),
+            "color.vertexlabels": ColorValue(Color.white()),
+            "color.vertices:is_fixed": ColorValue(Color.red()),
+            "color.vertices:line_constraint": ColorValue(Color.white()),
+            "color.edges": ColorValue(Color.black()),
+            "color.edges:is_ind": ColorValue(Color.cyan()),
+            "color.edges:is_external": ColorValue(Color.green()),
+            "color.edges:is_reaction": ColorValue(Color.green()),
+            "color.edges:is_load": ColorValue(Color.green()),
+            "color.edges:target_force": ColorValue(Color.white()),
+            "color.edges:target_vector": ColorValue(Color.white()),
+            "color.faces": ColorValue(Color.grey().lightened(50)),
+            "color.compression": ColorValue(Color.blue()),
+            "color.tension": ColorValue(Color.red()),
+            "scale.forces": FloatValue(1.0),
+            "tol.edges": FloatValue(0.01),
+            "tol.forces": FloatValue(0.01),
+        }
+    )
 
-    def __init__(self, diagram, *args, **kwargs):
-        super(FormObject, self).__init__(diagram, *args, **kwargs)
-        self.settings.update(FormObject.SETTINGS)
-        settings = kwargs.get("settings") or {}
-        if settings:
-            self.settings.update(settings)
+    def __init__(self, *args, **kwargs):
+        super(FormObject, self).__init__(*args, **kwargs)
