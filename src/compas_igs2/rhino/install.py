c6fa7ab7448816051849f7b9a92f735f82798a64
Licini <leochen1990@gmail.com>
2022-11-15 15:04:09 +0100

    post merge

--- src/compas_igs2/rhino/install.py
+++ src/compas_igs2/rhino/install.py
@@ -2,21 +2,6 @@ from compas.plugins import plugin
 
 
 @plugin(category="install", tryfirst=True)
-def after_rhino_install(installed_packages):
-    if "compas_ui" not in installed_packages:
-        return []
-    if "compas_cloud" not in installed_packages:
-        return []
-    if "compas_igs2" not in installed_packages:
-        return []
-
-    version = get_version_from_args()
-    install(version)
-
-    return [("compas_igs2", "Installed IGS2 UI", True)]
-
-
-@plugin(category="install", tryfirst=True)
 def installable_rhino_packages():
     return ["compas_igs2"]
 
