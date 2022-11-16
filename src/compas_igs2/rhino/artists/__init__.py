157d9b8e1afb50e3b8c65c5e2a7d4b139bab3854
brgcode <tom.v.mele@gmail.com>
2022-11-16 22:23:57 +0100

    avoid import error

--- src/compas_igs2/rhino/artists/__init__.py
+++ src/compas_igs2/rhino/artists/__init__.py
@@ -4,11 +4,12 @@ from __future__ import division
 
 from compas.plugins import plugin
 
-from compas_rhino.artists import RhinoArtist
+from compas.artists import Artist
 
 from compas_ags.diagrams import FormDiagram
 from compas_ags.diagrams import ForceDiagram
 
+from .diagramartist import RhinoDiagramArtist
 from .forceartist import RhinoForceArtist
 from .formartist import RhinoFormArtist
 
@@ -16,7 +17,7 @@ from .formartist import RhinoFormArtist
 @plugin(category="factories", requires=["Rhino"])
 def register_artists():
 
-    RhinoArtist.register(FormDiagram, RhinoFormArtist, context="Rhino")
-    RhinoArtist.register(ForceDiagram, RhinoForceArtist, context="Rhino")
+    Artist.register(FormDiagram, RhinoFormArtist, context="Rhino")
+    Artist.register(ForceDiagram, RhinoForceArtist, context="Rhino")
 
     print("IGS Rhino Artists registered.")
